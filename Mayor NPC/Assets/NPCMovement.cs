70707071f00272008ce355e46662b71a93577763
OzHache <OzHache@gmail.com>
2020-02-29 15:42:04 -0800

    second

---
+++ Mayor NPC/Assets/NPCMovement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour
+{
+    [SerializeField]private Transform target;
+    private Vector2 targetPos { get { return target.position; } }
+    [Range(0.1f, 1f)] public float speed = 1;
+    [Range(0.1f, 1f)] public float stoppingDistance =.5f;
+
+    //CALCULATED PROPERTIES
+    
+    private bool reachedTarget { get { return Vector2.Distance(transform.position, targetPos) < stoppingDistance; } }
+    
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (!reachedTarget)
+        {
+            MoveToLocation();
+            break;
+        }
+    }
+
+    private void MoveToLocation()
+    {xs
+        var step = speed * Time.deltaTime;
+        Vector2 moveDirection = Vector2.MoveTowards(transform.position, targetPos, step);
+        transform.position = moveDirection;
+    }
+
+  
+}
