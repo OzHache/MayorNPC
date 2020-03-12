6043001d367835d2c938e7567e1ef369fde5b8dd
Charles Hache MSI <OzHache@gmail.com>
2020-03-11 17:07:24 -0700

    Testing Changes
    Not needed.

---
+++ Mayor NPC/Assets/Scripts/TestingBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestingBehaviour
+{
+
+    private protected float _aPrivateFloat = 2;
+    
+
+}
+public class subTestingBehaviour : TestingBehaviour
+{
+    public float aPublicFloat { get { return base._aPrivateFloat; }
+        set { base._aPrivateFloat = value; } }
+
+    public void ChangeMyBasePrivateFloat (float constructorFloat)
+    {
+        base._aPrivateFloat = 1f;
+    }
+}
+
+public class testing
+{
+    public subTestingBehaviour STB = new subTestingBehaviour();
+    
+    void DoAThing()
+    {
+        Debug.Log(STB.aPublicFloat);
+    }
+}
